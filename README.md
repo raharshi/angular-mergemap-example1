b5e0550aa6985fcec22d8dc4fb7ed2b585c492cb
raharshi <raharshi.puli@gmail.com>
2020-04-04 19:38:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mergemap-example1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mergemap-example1)
---
+++ src/app/book.component.ts
@@ -0,0 +1,98 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+import { mergeMap, switchMap, catchError, map, delay, concatMap, exhaustMap } from 'rxjs/operators';
+import { BookService } from './book.service';
+import { Book } from './book';
+
+@Component({
+   selector: 'app-book',
+   templateUrl: '/book.component.html'
+})
+export class BookComponent implements OnInit { 
+  book: Book;
+  allBooks$: Observable<Book[]>;
+
+  constructor(private bookService: BookService) { }
+  ngOnInit() {
+    this.searchBook();
+    this.getAllBooks();
+  }
+
+  id = 102;
+  addMoreBooks() {
+    let book1 = new Book(++this.id, "Book-"+ this.id);
+    let book2 = new Book(++this.id, "Book-"+ this.id);
+    let book3 = new Book(++this.id, "Book-"+ this.id);
+    of(book1, book2, book3).pipe(
+      mergeMap(book => {
+        delay(2000);
+        return this.bookService.addBook(book);
+      }),
+      catchError(err => {
+        console.error(err.message);
+        return of(new Book(100, "Default Book"));
+      })
+    ).subscribe(book=>{
+      console.log(book.id+":"+book.name);
+      this.getAllBooks();
+    });
+  }
+  bookId = new FormControl(); 
+  searchBook() {
+  this.bookId.valueChanges.pipe(
+    switchMap(id => {
+      delay(2000);
+      if (id > this.id || id < 101) {
+        return of(null);
+      }
+      return this.bookService.getBookById(id);
+    })
+  ).subscribe(res => this.book = res);
+  }
+  getAllBooks() {
+    this.allBooks$ = this.bookService.getAllBooks();
+  }
+
+  getMerge(){
+    console.log("mergeMap");
+    of('x', 'y', 'z').pipe(
+    mergeMap(el => of(1, 2).pipe(
+          delay(2000),
+          map(num => el+num)        
+        )
+      )
+    ).subscribe(res => console.log(res));
+  }
+  getSwitch(){
+    console.log("switchMap");
+    of('x', 'y', 'z').pipe(
+    switchMap(el => of(1, 2).pipe(
+          delay(2000),
+          map(num => el+num)        
+        )
+      )
+    ).subscribe(res => console.log(res));
+  }
+  getConcat(){
+    console.log("concatMap");
+    of('x', 'y', 'z').pipe(
+    concatMap(el => of(1, 2).pipe(
+          delay(2000),
+          map(num => el+num)        
+        )
+      )
+    ).subscribe(res => console.log(res));
+  }
+
+  getExhaust(){
+    console.log("exhaustMap");
+    of('x', 'y', 'z').pipe(
+    exhaustMap(el => of(1, 2).pipe(
+          delay(2000),
+          map(num => el+num)        
+        )
+      )
+    ).subscribe(res => console.log(res));
+  }
+} 
